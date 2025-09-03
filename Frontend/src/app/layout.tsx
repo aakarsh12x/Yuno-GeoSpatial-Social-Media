d2818b65feb96ddc75deb7ba8be173775940ca0b
aakarsh12x <aakarshshrey12@gmail.com>
2025-09-04 03:08:23 +0530

    Remove manifest link to fix 401 error

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/icon.png" />
-        <link rel="manifest" href="/manifest.json?v=2" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
